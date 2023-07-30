26299ac266c0a3a8e224945675396fc879adc842
ChrisDaunt1984 <chris.daunt@nubis-communications.com>
2023-07-30 19:59:13 +0200

    handle request almost working

--- naturelifecert/pdf.py
+++ naturelifecert/pdf.py
@@ -1,120 +1,160 @@
-from flask import (
-    Blueprint, flash, g, redirect, render_template, request, url_for
-)
+from flask import Blueprint, flash, g, redirect, render_template, request, url_for, make_response, send_file
 from werkzeug.exceptions import abort
 
 from naturelifecert.auth import login_required
 from naturelifecert.db import get_db
 
-bp = Blueprint('pdf', __name__)
+from reportlab.lib.pagesizes import letter
+from reportlab.pdfgen import canvas
+import io
 
-@bp.route('/')
+bp = Blueprint("pdf", __name__)
+
+
+@bp.route("/")
 def index():
     db = get_db()
-    #Don't expose email address to the wild :)
+    # Don't expose email address to the wild :)
     posts = db.execute(
-        'SELECT p.id, first_name, last_name, country, donation, currency, created'
-        ' FROM post p JOIN user u ON p.author_id = u.id'
-        ' ORDER BY created DESC'
+        "SELECT p.id, first_name, last_name, country, donation, currency, created"
+        " FROM post p JOIN user u ON p.author_id = u.id"
+        " ORDER BY created DESC"
     ).fetchall()
-    return render_template('pdf/index.html', posts=posts)
+    return render_template("pdf/index.html", posts=posts)
 
-#TODO: Create and update could be bundled together
-@bp.route('/create', methods=('GET', 'POST'))
+
+# TODO: Create and update could be bundled together
+@bp.route("/create", methods=("GET", "POST"))
 @login_required
 def create():
-    print(request)
-    if request.method == 'POST':
-        first_name = request.form['first_name']
-        last_name =  request.form['last_name']
-        country =    request.form['country']
-        donation =   request.form['donation']
-        currency =   request.form['currency']
-        email =   request.form['email']
+    if request.method == "POST":
+        first_name = request.form["first_name"]
+        last_name = request.form["last_name"]
+        country = request.form["country"]
+        donation = request.form["donation"]
+        currency = request.form["currency"]
+        email = request.form["email"]
         error = None
 
-        for key in ['first_name',
-                    'last_name',
-                    'country',
-                    'donation',
-                    'currency',
-                    'email']:
+        for key in ["first_name", "last_name", "country", "donation", "currency", "email"]:
             if not locals()[key]:
-                #TODO: Check for real currency values
-                #TODO: Check if real email
-                error = f'{key} is required'
+                # TODO: Check for real currency values
+                # TODO: Check if real email
+                error = f"{key} is required"
 
         if error is not None:
             flash(error)
         else:
             db = get_db()
             db.execute(
-                'INSERT INTO post (first_name, last_name, country, donation, currency, author_id, created)'
-                ' VALUES (?, ?, ?, ?, ?, ?, CURRENT_TIMESTAMP)',
-                (first_name, last_name, country, donation, currency, g.user['id'])
+                "INSERT INTO post (first_name, last_name, country, donation, currency, author_id, created)"
+                " VALUES (?, ?, ?, ?, ?, ?, CURRENT_TIMESTAMP)",
+                (first_name, last_name, country, donation, currency, g.user["id"]),
             )
             db.commit()
-            return redirect(url_for('pdf.index'))
 
-    return render_template('pdf/create.html')
+            # Redirect to the generate route with the form data as URL parameters
+            return redirect(
+                url_for(
+                    "pdf.generate_pdf",
+                    first_name=first_name,
+                    last_name=last_name,
+                    country=country,
+                    donation=donation,
+                    currency=currency,
+                )
+            )
+
+    return render_template("pdf/create.html")
+
 
 def get_post(id, check_author=True):
-    post = get_db().execute(
-        'SELECT p.id, first_name, last_name, country, donation, currency, author_id'
-        ' FROM post p JOIN user u ON p.author_id = u.id'
-        ' WHERE p.id = ?',
-        (id,)
-    ).fetchone()
+    post = (
+        get_db()
+        .execute(
+            "SELECT p.id, first_name, last_name, country, donation, currency, author_id"
+            " FROM post p JOIN user u ON p.author_id = u.id"
+            " WHERE p.id = ?",
+            (id,),
+        )
+        .fetchone()
+    )
 
     if post is None:
         abort(404, f"Post id {id} doesn't exist.")
 
-    if check_author and post['author_id'] != g.user['id']:
+    if check_author and post["author_id"] != g.user["id"]:
         abort(403)
 
     return post
 
-@bp.route('/<int:id>/update', methods=('GET', 'POST'))
-@login_required
-def update(id):
-    post = get_post(id)
 
-    if request.method == 'POST':
-        first_name = request.form['first_name']
-        last_name = request.form['last_name']
-        country = request.form['country']
-        donation = request.form['donation']
-        currency = request.form['currency']
-        error = None
 
-        for key in ['first_name',
-                    'last_name',
-                    'country',
-                    'donation',
-                    'currency']:
-            if not locals()[key]:
-                #TODO: Check for real currency values
-                error = f'{key} is required'
 
-        if error is not None:
-            flash(error)
-        else:
-            db = get_db()
-            db.execute(
-                'UPDATE post SET first_name = ?, last_name = ?, country = ?, donation = ?, currency = ?, created=CURRENT_TIMESTAMP'
-                ' WHERE id = ?',
-                (first_name, last_name, country, donation, currency, id)
-            )
-            #db.commit()
-            return redirect(url_for('pdf.index'))
 
-    return render_template('pdf/update.html', post=post)
+# Your route to generate the PDF
+@bp.route("/generate_pdf", methods=["GET"])
+def generate_pdf():
+    # Retrieve form data from the URL parameters
+    first_name = request.args.get("first_name")
+    last_name = request.args.get("last_name")
+    country = request.args.get("country")
+    donation = request.args.get("donation")
+    currency = request.args.get("currency")
 
-@bp.route('/<int:id>/delete', methods=('POST',))
-@login_required
-def delete(id):
-    get_post(id)
-    db = get_db()
-    db.execute('DELETE FROM post WHERE id = ?', (id,))
-    db.commit()
-    return redirect(url_for('pdf.index'))
+    # Generate the PDF using reportlab with the retrieved form data
+    pdf_buffer = generate_pdf_from_data(first_name, last_name, country, donation, currency)
+
+    # Save the PDF to a temporary file
+    pdf_file_path = f'/tmp/form_data.pdf'
+    with open(pdf_file_path, 'wb') as pdf_file:
+        pdf_file.write(pdf_buffer)
+
+    # Set the PDF file path as a response variable to access it in the after_request function
+    response = make_response(redirect(url_for('index')))
+    response.pdf_file_path = pdf_file_path
+
+    return response
+
+# Your route to initiate the PDF download
+@bp.route('/download_pdf', methods=['GET'])
+def download_pdf():
+    # Get the file path of the generated PDF from the response variable
+    pdf_file_path = request.response.pdf_file_path
+
+    # Send the file as a response to initiate the download
+    return send_file(pdf_file_path, as_attachment=True)
+
+
+def generate_pdf_from_data(first_name, last_name, country, donation, currency):
+    buffer = io.BytesIO()
+    pdf_canvas = canvas.Canvas(buffer, pagesize=letter)
+
+    # Add content to the PDF using the form data
+    pdf_canvas.drawString(100, 700, "Form Data:")
+    pdf_canvas.drawString(100, 680, f"First Name: {first_name}")
+    pdf_canvas.drawString(100, 660, f"Last Name: {last_name}")
+    pdf_canvas.drawString(100, 640, f"Country: {country}")
+    pdf_canvas.drawString(100, 620, f"Donation: {donation}")
+    pdf_canvas.drawString(100, 600, f"Currency: {currency}")
+    # Add more content as needed
+
+    # Save the PDF
+    pdf_canvas.save()
+
+    buffer.seek(0)
+    return buffer.getvalue()
+
+@bp.after_request
+def handle_download_and_redirect(response):
+    # Check if the response has a pdf_file_path attribute
+    if hasattr(response, 'pdf_file_path'):
+        pdf_file_path = response.pdf_file_path
+
+        # Send the file as a response to initiate the download
+        response = send_file(pdf_file_path, as_attachment=True)
+
+        # Redirect back to the index page after the PDF is downloaded
+        return response, redirect(url_for('index'))
+
+    return response
